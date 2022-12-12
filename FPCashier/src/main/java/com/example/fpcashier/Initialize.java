71cd38973dd615c6c0330d251d5c0efb89f6a008
RichardRyan141 <114993335+RichardRyan141@users.noreply.github.com>
2022-12-12 17:07:22 +0700

    Delete FPCashier directory

--- FPCashier/src/main/java/com/example/fpcashier/Initialize.java
+++
@@ -1,8 +0,0 @@
-package com.example.fpcashier;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public interface Initialize {
-    public void initialize(URL location, ResourceBundle resources);
-}
--- FPCashier/src/main/java/com/example/fpcashier/ItemTypes.java
+++
@@ -1,8 +0,0 @@
-package com.example.fpcashier;
-
-public enum ItemTypes {
-    Food,
-    Drink,
-    Dessert,
-    Other
-}
